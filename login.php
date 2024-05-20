7abd171145e8dc964e3013a9e1c389180e7d454f
sashek06 <sashek06@gmail.com>
2024-05-20 15:13:21 +0500

    login done

--- login.php
+++ login.php
@@ -9,37 +9,7 @@
 	$con=connect();
 ?>
 
-<html data-bs-theme="dark">
-<head>
-	<meta charset="utf-8">
-	<title><?php echo $title;?></title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-
-<body>
-<table id="main_table" border="0">
-	<!-- баннер -->
-	<tr>
-		<td colspan=2 style="text-align:center">
-			<?php
-				include('top.php');
-			?>
-		</td>
-	</tr>
-
-	<tr>
-		<!-- меню -->
-		<td class='menu2'>
-			<?php
-				include('menu.php');
-			?>
-		</td>
-
-		<!-- контент -->
-		<td class="content">
-
 <?php
-
 	if(isset($_POST['submit'])){
 		$login=htmlentities(trim($_POST['user']), ENT_QUOTES, 'UTF-8');
 		$password=htmlentities(trim($_POST['pass']), ENT_QUOTES, 'UTF-8');
@@ -57,63 +27,52 @@
 		";
 		$res=mysqli_query($con, $query) or die(mysqli_error($con));
 		$row=mysqli_fetch_array($res, MYSQLI_ASSOC);
-		if (
-			(!empty($row['id']))
-		)	{ //успешный вход
-
+		if (!empty($row['id'])) { // успешный вход
 			$_SESSION['login'] =  $row['login'];
 			$_SESSION['level'] =  $row['level'];
 			$_SESSION['id'] =  $row['id'];
 			header("Location: index.php");
 			exit;
-		}
-		else
+		} else {
 			echo '<p>Логин или пароль неверны!</p>';
-	};
+		}
+	}
 ?>
-<html data-bs-theme="dark">
+
 <head>
-	<title>Авторизация</title>
-	<meta charset="UTF-8">
-	<link href="style.css" rel="stylesheet" />
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
-<body>
-<div align="center" >
-<form id="loginForm" method="post" style="width:500px; text-align:center">
-	<div class="field">
-		Имя пользователя:
-		<div class="input"><input type="text" name="user" value="" id="login" /></div>
+<?php
+    include('showcase.php');
+    include('menu.php');
+?>
+
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+
+<section class="login__form d-flex align-items-center h-100">
+	<div class="container">
+		<div class="row justify-content-center">
+			<form id="loginForm" method="post" class="col-4">
+				<div class="field mb-3">
+					<label class="form-label">Имя пользователя</label>
+					<div class="input"><input type="text" name="user" value="" id="login" class="form-control" /></div>
+				</div>
+				<div class="field mb-3">
+					<label class="form-label">Пароль</label>
+					<div class="input"><input type="password" name="pass" value="" id="pass" class="form-control" /></div>
+				</div>
+				<div class="submit d-flex justify-content-center">
+					<button type="submit" name="submit" class="btn btn-primary">Войти</button>
+				</div>
+			</form>
+		</div>
 	</div>
+</section>
 
-	<div class="field">
-		Пароль:
-		<div class="input"><input type="password" name="pass" value="" id="pass" /></div>
-	</div>
 
-	<div class="submit">
-		<button type="submit" name="submit">Войти</button>
-	</div>
-
-	<p">
-	Нет логина и пароля? <a href="reg.php">Регистрация</a>
-	</p>
-</form>
-
-</div>
-		</td>
-	</tr>
-
-	<!-- подвал -->
-	<tr>
-		<td colspan=2>
-			<?php
-				include('footer.php');
-			?>
-		</td>
-	</tr>
-
-</table>
-
-</body>
-</html>
+<?php
+    include('footer.php');
+?>
