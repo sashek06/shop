ded845e45678363f17169a65bf037d4b63e53c16
Rinnayb <rinnayb4@gmail.com>
2024-05-24 17:44:44 +0500

    redesign index.php

--- func.php
+++ func.php
@@ -1,99 +1,105 @@
 <?php
-$valuta=' р.';
+$valuta = ' р.';
 include 'database.php';
-function connect() {
-    global $hostname;
-    global $mysql_login;
-    global $mysql_password;
-    global $database;
-    $con = mysqli_connect($hostname, $mysql_login, $mysql_password, $database) or die(mysqli_error($con));
-    if (!$con) die('<h2>Ошибка подключения к серверу базы данных!</h2>');
-    mysqli_set_charset($con,'utf8') or die(mysqli_error($con));
-    return $con;
+function connect()
+{
+	global $hostname;
+	global $mysql_login;
+	global $mysql_password;
+	global $database;
+	$con = mysqli_connect($hostname, $mysql_login, $mysql_password, $database) or die(mysqli_error($con));
+	if (!$con) die('<h2>Ошибка подключения к серверу базы данных!</h2>');
+	mysqli_set_charset($con, 'utf8') or die(mysqli_error($con));
+	return $con;
 };
 
 // функция печатает результат запроса в виде html-таблицы
-function SQLResultTable($Query, $con, $mask) {
+function SQLResultTable($Query, $con, $mask)
+{
 
-    function mysqli_field_name($result, $field_offset) {
-        $properties = mysqli_fetch_field_direct($result, $field_offset);
-        return is_object($properties) ? $properties->name : null;
-    };
+	function mysqli_field_name($result, $field_offset)
+	{
+		$properties = mysqli_fetch_field_direct($result, $field_offset);
+		return is_object($properties) ? $properties->name : null;
+	};
 
-    $Table = "";  //initialize table variable
+	$Table = "";  //initialize table variable
 
-    $Table.= "<table class='table table-striped'>"; //Open HTML Table
+	$Table .= "<table class='table table-striped'>"; //Open HTML Table
 
-    $Result = mysqli_query($con, $Query); //Execute the query
-    if(mysqli_error($con)) {
-        $Table.= "<tr><td>MySQL ERROR: " . mysqli_error($con) . "</td></tr>";
-    }
-    else {
-        //Header Row with Field Names
-        $NumFields = mysqli_num_fields($Result);
-        $Table.= "<thead>";
-        $Table.= "<tr>";
-        for ($i=0; $i < $NumFields; $i++) {
-            $Table.= "<th scope='col'>" . mysqli_field_name($Result, $i) . "</th>";
-        }
-        $Table.= "</tr>";
-        $Table.= "</thead>";
+	$Result = mysqli_query($con, $Query); //Execute the query
+	if (mysqli_error($con)) {
+		$Table .= "<tr><td>MySQL ERROR: " . mysqli_error($con) . "</td></tr>";
+	} else {
+		//Header Row with Field Names
+		$NumFields = mysqli_num_fields($Result);
+		$Table .= "<thead>";
+		$Table .= "<tr>";
+		for ($i = 0; $i < $NumFields; $i++) {
+			$Table .= "<th scope='col'>" . mysqli_field_name($Result, $i) . "</th>";
+		}
+		$Table .= "</tr>";
+		$Table .= "</thead>";
 
-        //Loop thru results
-        $Table.= "<tbody>";
-        $RowCt = 0; //Row Counter
-        while($Row = mysqli_fetch_assoc($Result)) {
-            $Table.= "<tr>";
-            //Loop thru each field
-            foreach($Row as $field => $value) {
-                // делаем подсветку найденного
-                $value = str_replace($mask, "<font color='red'>$mask</font>", $value);
-                // отображаем значение
-                if ($RowCt % $NumFields == 0) {
-                    $Table .= "<th scope='row'>$value</th>";
-                } else {
-                    $Table .= "<td>$value</td>";
-                }
-            }
-            $Table.= "</tr>";
-            $RowCt++;
-        }
-    }
-    $Table.= "</tbody>";
-    $Table.= "</table>";
+		//Loop thru results
+		$Table .= "<tbody>";
+		$RowCt = 0; //Row Counter
+		while ($Row = mysqli_fetch_assoc($Result)) {
+			$Table .= "<tr>";
+			//Loop thru each field
+			foreach ($Row as $field => $value) {
+				// Проверяем, что $value не null
+				if ($value !== null) {
+					// делаем подсветку найденного
+					$value = str_replace($mask, "<font color='red'>$mask</font>", $value);
+				}
+				// отображаем значение
+				if ($RowCt % $NumFields == 0) {
+					$Table .= "<th scope='row'>" . ($value !== null ? $value : '') . "</th>";
+				} else {
+					$Table .= "<td>" . ($value !== null ? $value : '') . "</td>";
+				}
+			}
+			$Table .= "</tr>";
+			$RowCt++;
+		}
+	}
+	$Table .= "</tbody>";
+	$Table .= "</table>";
 
-    return $Table;
+	return $Table;
 };
 
-function can_upload($file){
-    // если имя пустое, значит файл не выбран
-    if($file['name'] == '')
-        return 'Вы не выбрали файл.';
+function can_upload($file)
+{
+	// если имя пустое, значит файл не выбран
+	if ($file['name'] == '')
+		return 'Вы не выбрали файл.';
 
-    /* если размер файла 0, значит его не пропустили настройки
+	/* если размер файла 0, значит его не пропустили настройки
     сервера из-за того, что он слишком большой */
-    if($file['size'] == 0)
-        return 'Файл слишком большой.';
+	if ($file['size'] == 0)
+		return 'Файл слишком большой.';
 
-    // разбиваем имя файла по точке и получаем массив
-    $getMime = explode('.', $file['name']);
-    // нас интересует последний элемент массива - расширение
-    $mime = strtolower(end($getMime));
-    // объявим массив допустимых расширений
-    $types = array('jpg', 'png', 'gif', 'bmp', 'jpeg');
+	// разбиваем имя файла по точке и получаем массив
+	$getMime = explode('.', $file['name']);
+	// нас интересует последний элемент массива - расширение
+	$mime = strtolower(end($getMime));
+	// объявим массив допустимых расширений
+	$types = array('jpg', 'png', 'gif', 'bmp', 'jpeg');
 
-    // если расширение не входит в список допустимых - return
-    if(!in_array($mime, $types))
-        return 'Недопустимый тип файла.';
+	// если расширение не входит в список допустимых - return
+	if (!in_array($mime, $types))
+		return 'Недопустимый тип файла.';
 
-    return true;
+	return true;
 }
 
-function make_upload($file, $id){
-    // формируем уникальное имя картинки: случайное число и name
-    $name = $id.'.jpg';
-    copy($file['tmp_name'], 'upload/' . $name);
+function make_upload($file, $id)
+{
+	// формируем уникальное имя картинки: случайное число и name
+	$name = $id . '.jpg';
+	copy($file['tmp_name'], 'upload/' . $name);
 }
 
 error_reporting(0);
-?>
--- view.php
+++ view.php
@@ -16,6 +16,7 @@ include "database.php";
 include "func.php";
 include "styles.php";
 include "scripts.php";
+include "menu.php";
 $con = connect();
 $title = 'Товары';
 $table = 'products';
@@ -27,25 +28,25 @@ $table = 'products';
 ?>
 
 <section class="cart mt-5" style="min-height: 100vh;">
-<div class="container">
-	<div class="h1 text-center">		
-<?php
-    $cat_id=empty($_GET['cat_id']) ? '' : abs(intval($_GET['cat_id']));
-    if ($cat_id) {
-        // если выбрана категория
-        $query="
+    <div class="container">
+        <div class="h1 text-center">
+            <?php
+            $cat_id = empty($_GET['cat_id']) ? '' : abs(intval($_GET['cat_id']));
+            if ($cat_id) {
+                // если выбрана категория
+                $query = "
             SELECT name
             FROM categories
             WHERE 1
                 AND id=$cat_id
         ";
-        $res=mysqli_query($con, $query) or die(mysqli_error($con));
-        $row=mysqli_fetch_array($res);
-        $cat_name=$row['name'];
-        echo "$cat_name</div>";
-    };
-    $filter_cat= $cat_id==0 ? '' : "AND `$table`.`cat_id`='$cat_id'"; // если категория не выбрана, показать все товары
-    $query="
+                $res = mysqli_query($con, $query) or die(mysqli_error($con));
+                $row = mysqli_fetch_array($res);
+                $cat_name = $row['name'];
+                echo "$cat_name</div>";
+            };
+            $filter_cat = $cat_id == 0 ? '' : "AND `$table`.`cat_id`='$cat_id'"; // если категория не выбрана, показать все товары
+            $query = "
     SELECT t.*
     FROM (
         SELECT
@@ -77,80 +78,73 @@ $table = 'products';
         LIMIT 50) AS t
     WHERE amount>0;
     ";
-    $res=mysqli_query($con, $query) or die(mysqli_error($con));
+            $res = mysqli_query($con, $query) or die(mysqli_error($con));
 
-    // собираем данные в массив
-    $a=array();
-    while ($row=mysqli_fetch_array($res, MYSQLI_ASSOC)) {
-        $a[]=$row;
-    };
+            // собираем данные в массив
+            $a = array();
+            while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+                $a[] = $row;
+            };
 
-    // вывод в таблицу
-    $in_row=3; // сколько столбцов товаров
-    // количество строк
-    $row_count=ceil(count($a)/$in_row);
-    echo '<div class="row justify-content-center">';
-    for($i=1; $i<=$row_count; $i++) {
-        for($j=1; $j<=$in_row; $j++) {
-            $ind=($i-1)*$in_row+$j-1;
-            if (isset($a[$ind])) {
-                $row=$a[$ind];
-                $fname='images/'.$row['id'].'.jpg';
-                if (!file_exists($fname)) { // если нет файла, показать "НЕТ ФОТО"
-                    $fname='images/stock-goods.webp';
+            // вывод в таблицу
+            $in_row = 3; // сколько столбцов товаров
+            // количество строк
+            $row_count = ceil(count($a) / $in_row);
+            echo '<div class="row justify-content-center">';
+            for ($i = 1; $i <= $row_count; $i++) {
+                for ($j = 1; $j <= $in_row; $j++) {
+                    $ind = ($i - 1) * $in_row + $j - 1;
+                    if (isset($a[$ind])) {
+                        $row = $a[$ind];
+                        $fname = 'images/' . $row['id'] . '.jpg';
+                        if (!file_exists($fname)) { // если нет файла, показать "НЕТ ФОТО"
+                            $fname = 'images/stock-goods.webp';
+                        };
+
+                        if ($row['delta'] < 30) { // товар добавлен меньше 30 дней назад, т.е. это новинка
+                            $new = "";
+                        } else {
+                            $new = '';
+                        };
+
+                        if ($row['discount_value']) { // цена со скидкой
+                            $price_new = number_format(round($row['price'] * (1 - $row['discount_value'] / 100), 2), 2, '.', '');
+                            $price_str = "
+                        <font style='color: #888; font-size:small; text-decoration:line-through'>$row[price]$valuta</font><br>
+                        $price_new$valuta
+                    ";
+                            $price_str = trim($price_str);
+                        } else {
+                            $price_str = "$row[price]$valuta";
+                        };
+
+                        // обрезать описание, если оно очень длинное
+                        if (mb_strlen($row['descr'], 'UTF-8') > 50) {
+                            $descr = mb_substr($row['descr'], 0, 50, 'UTF-8') . '...';
+                        } else {
+                            $descr = $row['descr'];
+                        };
+
+                        echo "
+                    <div class='card my-3 mx-3 px-0 col-xl-3 col-lg-4 col-md-6 col-sm-8 col-xs-8'>
+                        <img src='$fname' class='card-img-top' alt='$row[name]' style='cursor:pointer;' onclick='to_cart($row[id]);'>
+                        <div class='card-body'>
+                            <h5 class='card-title'>$row[name]</h5>
+                            <p class='card-text'>$price_str</p>
+							<div class='buttons d-flex justify-content-around'>
+								<a href='card.php?product_id=$row[id]' class='btn btn-primary'>Подробнее</a>
+                            	<button onclick='to_cart($row[id]);' class='btn btn-success'>В корзину</button>
+							</div>
+                        </div>
+                    </div>
+                ";
+                    };
                 };
-
-                if ($row['delta']<30) { // товар добавлен меньше 30 дней назад, т.е. это новинка
-                    $new="";
-                }
-                else {
-                    $new='';
-                };
-
-				if ($row['discount_value']) { // цена со скидкой
-					$price_new=number_format (round($row['price']*(1-$row['discount_value']/100), 2), 2, '.', '');
-					$price_str="
-						<font style='color: #888; font-size:x-small; text-decoration:line-through'>$row[price]$valuta</font>
-						<img src='images/discount.png' height='24px' title='Скидка'>
-						<font style='color: #000;'>$price_new$valuta</font>
-					";
-					$price_str=trim($price_str);
-				}
-				else {
-					$price_str="<font style='color: #000;'>$row[price]$valuta</font>";
-				};
-
-				// обрезать описание, если оно очень длинное
-				if (mb_strlen($row['descr'], 'UTF-8')>50) {
-					$descr=mb_substr($row['descr'], 0, 50, 'UTF-8').'...';
-				}
-				else {
-					$descr=$row['descr'];
-				};
-				echo "
-				<td style='width:400px; height:400px'>
-					$new
-					Наименование: <b><a href='card.php?product_id=$row[id]'>$row[name]</a></b><br>
-					Описание: $descr<br>
-					Цена: $price_str<br>
-					Осталось: $row[amount] шт.<br>
-					<img src=\"$fname\" width=\"250px\" height=\"250px\" style='cursor:pointer;' onclick='to_cart($row[id]);'><br>
-					<button onclick='to_cart($row[id]);'>В корзину</button>
-				</td>
-				";
-			};
-		};
-		echo "</tr>";
-	};
-	echo '</table>';
-
-
-?>
-
-		</td>
-	</tr>
-
-</table>
+            };
+            echo '</div>';
+            ?>
+        </div>
+</section>
 
 <?php
 include('footer.php');
